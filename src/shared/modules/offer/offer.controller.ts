5688a4ee249e836b0f9663c250eccfba156333e6
Adonariz <raikkonen07@live.ru>
2026-04-20 23:09:03 +0300

    adds offer show method

--- src/shared/modules/offer/offer.controller.ts
+++ src/shared/modules/offer/offer.controller.ts
@@ -1,10 +1,15 @@
 import { StatusCodes } from 'http-status-codes';
 import { inject, injectable } from 'inversify';
 import { Request, Response } from 'express';
-import { BaseController, HttpError } from '../../libs/rest/index.js';
+import {
+  BaseController,
+  HttpError,
+  HttpMethod,
+} from '../../libs/rest/index.js';
 import { Component } from '../../types/index.js';
 import { Logger } from '../../libs/logger/index.js';
 import { OfferService } from './offer-service.interface.js';
+import { ParamOfferId } from './types/param-offerid.type.js';
 
 @injectable()
 export class OfferController extends BaseController {
@@ -14,13 +19,40 @@ export class OfferController extends BaseController {
     protected readonly offerService: OfferService,
   ) {
     super(logger);
+
+    this.logger.info('Register routes for OfferController...');
+    this.addRoute({
+      path: '/:offerId',
+      method: HttpMethod.Get,
+      handler: this.show,
+    });
   }
 
-  public async list(_req: Request, _res: Response): Promise<void> {
-    throw new HttpError(
-      StatusCodes.NOT_IMPLEMENTED,
-      'Not implemented',
-      'OfferController',
-    );
+  public async show(
+    { params }: Request<ParamOfferId>,
+    res: Response,
+  ): Promise<void> {
+    const { offerId } = params;
+
+    // TOFO: разобраться с юнион типом ParamOfferId
+    if (Array.isArray(offerId)) {
+      throw new HttpError(
+        StatusCodes.BAD_REQUEST,
+        'Invalid offerId format',
+        'OfferController',
+      );
+    }
+
+    const offer = await this.offerService.findById(offerId);
+
+    if (!offer) {
+      throw new HttpError(
+        StatusCodes.NOT_FOUND,
+        `Offer with id ${offerId} not found.`,
+        'OfferController',
+      );
+    }
+
+    this.ok(res, offer);
   }
 }
