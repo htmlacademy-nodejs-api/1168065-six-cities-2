85093a721ad20611c9d133cb451878b292cb9d3f
Alexey Zhila <raikkonen07@live.ru>
2026-04-20 17:48:46 +0300

    adds http error handling to exception filter

--- src/shared/libs/rest/exception-filter/app-exception-filter.ts
+++ src/shared/libs/rest/exception-filter/app-exception-filter.ts
@@ -1,9 +1,11 @@
+import { Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import { inject, injectable } from 'inversify';
 import { ExceptionFilter } from './exception-filter.interface.js';
 import { Component } from '../../../types/index.js';
 import { Logger } from '../../logger/index.js';
-import { Request, Response, NextFunction } from 'express';
-import { StatusCodes } from 'http-status-codes';
+import { HttpError } from '../errors/index.js';
+import { createErrorObject } from '../../../helpers/index.js';
 
 @injectable()
 export class AppExceptionFilter implements ExceptionFilter {
@@ -11,15 +13,41 @@ export class AppExceptionFilter implements ExceptionFilter {
     this.logger.info('Register AppExceptionFilter');
   }
 
-  public catch(
+  private handleHttpError(
+    error: HttpError,
+    _req: Request,
+    res: Response,
+    _next: NextFunction,
+  ) {
+    this.logger.error(
+      `[${error.detail}]: ${error.httpStatusCode} — ${error.message}`,
+      error,
+    );
+    res.status(error.httpStatusCode).json(createErrorObject(error.message));
+  }
+
+  private handleOtherError(
     error: Error,
+    _req: Request,
+    res: Response,
+    _next: NextFunction,
+  ) {
+    this.logger.error(error.message, error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(createErrorObject(error.message));
+  }
+
+  public catch(
+    error: Error | HttpError,
     req: Request,
     res: Response,
     next: NextFunction,
   ): void {
-    this.logger.error(error.message, error);
-    res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ error: error.message });
+    if (error instanceof HttpError) {
+      return this.handleHttpError(error, req, res, next);
+    }
+
+    return this.handleOtherError(error, req, res, next);
   }
 }
