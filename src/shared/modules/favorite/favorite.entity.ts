cb79fad3038e5bd41456c8fd29039fa9942ea135
Adonariz <raikkonen07@live.ru>
2026-05-02 15:48:38 +0300

    adds favorite entity

---
+++ src/shared/modules/favorite/favorite.entity.ts
@@ -0,0 +1,37 @@
+import {
+  prop,
+  index,
+  getModelForClass,
+  Ref,
+  modelOptions,
+  defaultClasses,
+} from '@typegoose/typegoose';
+import { UserEntity } from '../user/index.js';
+import { OfferEntity } from '../offer/index.js';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export interface FavoriteEntity extends defaultClasses.Base {}
+
+@modelOptions({
+  schemaOptions: {
+    collection: 'favorites',
+    timestamps: true,
+  },
+})
+@index({ userId: 1, offerId: 1 }, { unique: true })
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export class FavoriteEntity extends defaultClasses.TimeStamps {
+  @prop({
+    ref: UserEntity,
+    required: true,
+  })
+  public userId!: Ref<UserEntity>;
+
+  @prop({
+    ref: OfferEntity,
+    required: true,
+  })
+  public offerId!: Ref<OfferEntity>;
+}
+
+export const FavoriteModel = getModelForClass(FavoriteEntity);
