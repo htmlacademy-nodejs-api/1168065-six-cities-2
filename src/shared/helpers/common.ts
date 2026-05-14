4ee6d0a6353c369133d66940412be56fc58eb70f
Adonariz <raikkonen07@live.ru>
2026-05-14 20:52:58 +0300

    adds validation error

--- src/shared/helpers/common.ts
+++ src/shared/helpers/common.ts
@@ -1,4 +1,6 @@
 import { ClassConstructor, plainToInstance } from 'class-transformer';
+import { ValidationError } from 'class-validator';
+import { ValidationErrorField } from '../libs/rest/index.js';
 
 export function getErrorMessage(error: unknown): string {
   return error instanceof Error ? error.message : '';
@@ -14,4 +16,14 @@ export function fillDTO<T, V>(someDto: ClassConstructor<T>, plainObject: V) {
   return plainToInstance(someDto, plainObject, {
     excludeExtraneousValues: true,
   });
+}
+
+export function reduceValidationErrors(
+  errors: ValidationError[],
+): ValidationErrorField[] {
+  return errors.map(({ property, value, constraints }) => ({
+    property,
+    value,
+    messages: constraints ? Object.values(constraints) : [],
+  }));
 }
