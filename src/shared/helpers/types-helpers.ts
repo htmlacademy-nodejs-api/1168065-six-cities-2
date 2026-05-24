a4f1363961c4bcbbd1431d60019affe1c68fb8a5
Adonariz <raikkonen07@live.ru>
2026-05-24 22:39:00 +0300

    removes unused type guards and helper

--- src/shared/helpers/types-helpers.ts
+++ src/shared/helpers/types-helpers.ts
@@ -1,9 +1,3 @@
-export function createStringUnionGuard<T extends readonly string[]>(values: T) {
-  const set = new Set(values);
-
-  return (value: string): value is T[number] => set.has(value as T[number]);
-}
-
 export function createStringUnionParser<T extends readonly string[]>(
   values: T,
 ) {
